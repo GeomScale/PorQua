46170b4fa5ac720e0764b5dec6ab076e8e6c1c9f
tmhho <hthiminhha@gmail.com>
2024-05-31 14:19:40 +0200

    seperate code into src qnd test

--- test/tests_quadratic_program.py
+++ test/tests_quadratic_program.py
@@ -20,10 +20,10 @@ from qpsolvers import solve_qp
 from typing import Tuple
 
 
-from helper_functions import *
-from covariance import Covariance
-from optimization import *
-from optimization_data import OptimizationData
+from src.helper_functions import *
+from src.covariance import Covariance
+from src.optimization import *
+from src.optimization_data import OptimizationData
 
 
 
@@ -31,12 +31,12 @@ from optimization_data import OptimizationData
 
 
 class TestQuadraticProgram(unittest.TestCase):
-    
+
     def __init__(self, universe = 'msci', solver_name = 'cvxopt'):
         self._universe = universe
         self._solver_name = solver_name
         self.load_data(universe = universe)
-        
+
     def load_data(self, universe = 'msci') -> None:
         if universe == 'msci':
             self.data = load_data_msci()
@@ -46,26 +46,26 @@ class TestQuadraticProgram(unittest.TestCase):
             raise ValueError('Universe not recognized.')
         return None
 
-   
-    
+
+
 # --------------------------------------------------------------------------
 class TestLeastSquares(TestQuadraticProgram):
-    
+
     def prep_optim(self, rebdate: str = None) -> None:
-        
-        
+
+
         # Initialize optimization object
         optim = LeastSquares(solver_name = self._solver_name)
-        
+
         # Add constraints
         optim.constraints = Constraints(selection = self.data['X'].columns)
         optim.constraints.add_box(box_type = 'LongOnly')
         optim.constraints.add_budget()
-        
+
         # Set objective
         optimization_data = OptimizationData(X = self.data['X'], y = self.data['y'], align = True)
         optim.set_objective(optimization_data = optimization_data)
-        
+
         # Ensure that P and q are numpy arrays
         if 'P' in optim.objective.keys():
             if hasattr(optim.objective['P'], "to_numpy"):
@@ -74,33 +74,32 @@ class TestLeastSquares(TestQuadraticProgram):
             raise ValueError("Missing matrix 'P' in objective.")
         if 'q' in optim.objective.keys():
             if hasattr(optim.objective['q'], "to_numpy"):
-                optim.objective['q'] = optim.objective['q'].to_numpy()       
+                optim.objective['q'] = optim.objective['q'].to_numpy()
         else:
             optim.objective['q'] = np.zeros(len(optim.constraints.selection))
-        
+
         # Initialize the optimization model
         optim.model_qpsolvers()
-        
+
         # Attach the optimization object to self
         self.optim = optim
-        
+
         return None
-      
-    
+
+
 
 
 def test_1():
-    
+
     test = TestLeastSquares(universe = 'msci', solver_name = 'cvxopt')
     test.prep_optim()
     test.optim.solve()
-    
+
     test.optim.model
     test.optim.results
-  
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
