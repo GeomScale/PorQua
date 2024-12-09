8c98dcbf740fe2894a0ffd56141966f6f27ef96a
huuphuocle <dota.atmer@gmail.com>
2024-12-09 07:51:13 +0100

    Use MSCI data for test_portfolio

--- test/test_portfolio.py
+++ test/test_portfolio.py
@@ -3,8 +3,8 @@ import unittest
 import pandas as pd
 sys.path.insert(1, 'src')
 
-from portfolio import Portfolio
-from portfolio import Strategy
+from data_loader import load_data_msci
+from portfolio import Portfolio, Strategy
 
 
 
@@ -12,13 +12,13 @@ class TestPortfolio(unittest.TestCase):
 
     def __init__(self, testname):
         super().__init__(testname)
-        prices = pd.read_csv('data/dummy_data.csv', index_col=0, parse_dates=True)
-        self.return_series = prices.pct_change(1).dropna()
+        data = load_data_msci(path=None, n=3)
+        self.return_series = data['return_series']
 
     def test_portfolio(self):
         tickers = self.return_series.columns
 
-        timeline = ['2024-08-30', '2024-09-30', '2024-10-31']
+        timeline = ['2022-08-30', '2022-09-30', '2022-10-31']
         weights = [{tickers[0]: 0.3, tickers[1]: 0.7},
                    {tickers[0]: 0.2, tickers[1]: 0.4, tickers[2]: 0.4},
                    {tickers[1]: 0.5, tickers[2]: 0.5}]
@@ -27,13 +27,13 @@ class TestPortfolio(unittest.TestCase):
         portfolios = [Portfolio(date, weight) for date, weight in zip(timeline, weights)]
 
         # test float weight
-        w_float = portfolios[0].float_weights(self.return_series, '2024-10-01')
+        w_float = portfolios[0].float_weights(self.return_series, '2022-10-01')
 
         # create a strategy
         strategy = Strategy(portfolios)
 
         weights_df = strategy.get_weights_df()
-        weights_at_date = strategy.get_weights('2024-09-30')
+        weights_at_date = strategy.get_weights('2022-09-30')
         turnover = strategy.turnover(self.return_series, True)
         portf_returns = strategy.simulate(self.return_series)
         self.assertEqual(turnover.iloc[0], 0.8)
